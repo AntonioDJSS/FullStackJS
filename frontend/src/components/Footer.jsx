a6ffc1db4c39e3e78dbb099ae20b37e2731bf930
AntonioDJSS <antoniodjss2000@gmail.com>
2022-05-25 00:54:33 -0500

    Front

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="py-10">
+      <p className="text-center font-bold">
+        IKTAN Training - Administrador de Alta de
+        {' '}
+        <span className="text-indigo-600">Constancias</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
